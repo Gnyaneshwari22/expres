1c4cbba3e8ab4a51a9254fe997b511d572035f76
Gnyaneshwari22 <gnyaneshwariadki2@gmail.com>
2025-02-04 17:28:11 +0530

    body-parser added

--- index.js
+++ index.js
@@ -1,19 +1,27 @@
 const http = require("http");
 const express = require("express");
 //const server = http.createServer(app);
+const bodyParser = require("body-parser");
 
 const app = express();
 
-app.use((req, res, next) => {
-  console.log("In the frst middle ware");
-  next();
+app.use(bodyParser.urlencoded({ extended: false }));
+
+app.use("/add-product", (req, res, next) => {
+  res.send(
+    `<form action="/product" method="POST" name="add-product"><label>Name:</lable><input type="text" name="title" /><br></br><label>Number:</lable><input type="number" name="numberofprod" /><br></br><button type="submit">SUBMIT</button></form>`
+  );
 });
 
-app.use((req, res, next) => {
-  console.log("In the second middle ware");
-  res.send(`<h1>Welcome to Express JS</h1>`);
+app.post("/product", (req, res, next) => {
+  console.log(req.body);
+  res.redirect("/");
+});
+
+app.use("/", (req, res, next) => {
+  res.send(`<h1>Welcome to Express home JS</h1>`);
 });
 
 app.listen(4000, () => {
-  console.log("Server started on port 3000");
+  console.log("Server started on port 4000");
 });
